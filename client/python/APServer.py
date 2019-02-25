0f8099b06d29dfd8ebff067e1b2bb87f9ec6fff2
Jäger <RJAEGER@controlware.de>
2019-02-25 17:43:06 +0100

    recherche anima

--- client/python/APServer.py
+++ client/python/APServer.py
@@ -1,8 +1,8 @@
 import sys
 
 #import GRASP to use its thread safe print function
-sys.path.insert(0, 'graspy/')
-from grasp import tprint
+#sys.path.insert(0, 'graspy/')
+#from grasp import tprint
 
 from _thread import start_new_thread
 
@@ -11,14 +11,14 @@ import GRASP_RegServer
 import TLS_Server
 
 def main(args):
-    tprint("starting main Thread")
+    print("starting main Thread")
 
     start_new_thread(TLS_Server.main,())
     start_new_thread(GRASP_RegServer.main, (1,))
 
     while(True):
         pass
-        
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
